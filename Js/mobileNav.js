a7c9b126d13aae5e5ac72893784bd9a22d2f157b
melashu <meshu102@outlook.com>
2022-07-19 00:31:57 +0300

    Add close button implemntion

--- Js/mobileNav.js
+++ Js/mobileNav.js
@@ -9,29 +9,39 @@ let devName = document.getElementsByClassName("dev-name")[0];
 let litsItem = document.querySelector("nav ul");
 
 menuIcon.addEventListener("click", () => {
-    /***
-     * Lets hidden some element
-     */
-    menuIcon.style.display = "none";
-    mainContainer.style.display = "none";
-    devName.style.display = "none";
-    mainBox.style.display = "none";
-  
-    btnClose.style.display = "block";
-  
-    headerContainer.style.height = "100vh";
-    headerContainer.style.alignItems = "flex-start";
-  
-    navContainer.style.display = "flex";
-    navContainer.style.flexDirection = "column";
-    navContainer.style.justifyContent = "space-between";
-    navContainer.style.width = "100%";
-  
-    litsItem.style.display = "block";
-    litsItem.style.alignItems = "initial";
-    litsItem.style.width = "90%";
-    litsItem.style.color = "#fff5e1";
-  });
-  
+  /***
+   * Lets hidden some element
+   */
+  menuIcon.style.display = "none";
+  mainContainer.style.display = "none";
+  devName.style.display = "none";
+  mainBox.style.display = "none";
 
+  btnClose.style.display = "block";
 
+  headerContainer.style.height = "100vh";
+  headerContainer.style.alignItems = "flex-start";
+
+  navContainer.style.display = "flex";
+  navContainer.style.flexDirection = "column";
+  navContainer.style.justifyContent = "space-between";
+  navContainer.style.width = "100%";
+
+  litsItem.style.display = "block";
+  litsItem.style.alignItems = "initial";
+  litsItem.style.width = "90%";
+  litsItem.style.color = "#fff5e1";
+});
+
+btnClose.addEventListener("click", () => {
+  menuIcon.style.display = "block";
+  mainContainer.style.display = "block";
+  devName.style.display = "block";
+  mainBox.style.display = "block";
+
+  btnClose.style.display = "none";
+
+  navContainer.style.width = "auto";
+  litsItem.style.display = "none";
+
+});
