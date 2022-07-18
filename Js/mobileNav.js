7c2a028eb9b142b2ce6efcf446943677f678bf23
Rikstone <tarikbouari@gmail.com>
2022-07-18 19:08:32 +0000

    target html element

--- Js/mobileNav.js
+++ Js/mobileNav.js
@@ -1,2 +1,9 @@
 let mainContainer = document.querySelector("main");
 let headerContainer = document.querySelector("header");
+
+let navContainer = document.getElementsByClassName("nav-bar")[0];
+let mainBox = document.getElementById("mail-box");
+let menuIcon = document.getElementById("menu-icon");
+let btnClose = document.getElementById("btn-close");
+let devName = document.getElementsByClassName("dev-name")[0];
+let litsItem = document.querySelector("nav ul");
