9d997df8c04fe9ec2b061f87d8648c7acf981ee5
Rikstone <tarikbouari@gmail.com>
2022-07-18 19:17:01 +0000

    link script file

--- Js/mobileNav.js
+++ Js/mobileNav.js
@@ -7,3 +7,31 @@ let menuIcon = document.getElementById("menu-icon");
 let btnClose = document.getElementById("btn-close");
 let devName = document.getElementsByClassName("dev-name")[0];
 let litsItem = document.querySelector("nav ul");
+
+menuIcon.addEventListener("click", () => {
+    /***
+     * Lets hidden some element
+     */
+    menuIcon.style.display = "none";
+    mainContainer.style.display = "none";
+    devName.style.display = "none";
+    mainBox.style.display = "none";
+  
+    btnClose.style.display = "block";
+  
+    headerContainer.style.height = "100vh";
+    headerContainer.style.alignItems = "flex-start";
+  
+    navContainer.style.display = "flex";
+    navContainer.style.flexDirection = "column";
+    navContainer.style.justifyContent = "space-between";
+    navContainer.style.width = "100%";
+  
+    litsItem.style.display = "block";
+    litsItem.style.alignItems = "initial";
+    litsItem.style.width = "90%";
+    litsItem.style.color = "#fff5e1";
+  });
+  
+
+
