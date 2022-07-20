084e15462f32749f066f38195529b1ec62015ef6
Rikstone <tarikbouari@gmail.com>
2022-07-20 13:42:10 +0000

    add array object

---
+++ Js/popup.js
@@ -0,0 +1,93 @@
+const projectList = [
+    {
+      projectTitle: "Multi-Post Stories",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/featuer.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Profesional Art Printing Data",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card1.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Data Dashboard Healthcare",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card2.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Profesional Art Printing Data",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card3.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Profesional Art Printing Data",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card4.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Profesional Art Printing Data",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card33.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+    {
+      projectTitle: "Profesional Art Printing Data",
+      projectDiscription:
+        " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+      techTools: {
+        html: "HTML",
+        bootstrap: "Bootstrap",
+        ruby: "Ruby",
+      },
+      projectImage: "../image/card22.png",
+      liveLink: "https://melashu.github.io/My-Portfolio/",
+      liveSources: "https://github.com/melashu/My-Portfolio",
+    },
+  ];
