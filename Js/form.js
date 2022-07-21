194dd962a8306f7c6287558874e1df96c620070c
melashu <meshu102@outlook.com>
2022-07-21 13:38:38 +0300

    Fix linter error

--- Js/form.js
+++ Js/form.js
@@ -1,15 +1,14 @@
-const form = document.getElementsByClassName("form")[0];
-const email = document.getElementById("email");
-const errorMessage = document.getElementsByClassName("error-message")[0];
+const form = document.getElementsByClassName('form')[0];
+const email = document.getElementById('email');
+const errorMessage = document.getElementsByClassName('error-message')[0];
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (email.value === email.value.toLowerCase()) {
     form.submit();
   } else {
     email.focus();
-    errorMessage.innerHTML =
-      "The form in not sent, Please enter lowercase email look likes above!";
+    errorMessage.innerHTML = 'The form in not sent, Please enter lowercase email look likes above!';
     email.value = email.value.toLowerCase();
   }
 });
